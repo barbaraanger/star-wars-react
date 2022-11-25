18280686d08ac241b078bd5266a422b5df600b58
Bárbara Anger <barbaraanger@MacBook-Pro-de-Barbara.local>
2022-11-25 02:37:55 -0300

    create new components to work with routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Layout from './components/Header/Header'
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  HashRouter
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Layout/>
-    </div>
+    <HashRouter>
+      <div className="App">
+        <Layout />
+      </div>
+    </HashRouter>
   );
 }
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,37 @@
 import React from "react";
-import { Nav } from "react-bootstrap";
+import { Nav, NavItem } from "react-bootstrap";
+import { NavLink, Route, Routes } from "react-router-dom";
+import Characters from "../Characters/Characters";
+import Container from "../Container/Container";
+import Planets from "../Planets/Planets";
 import { headerStyles } from "./Header.styles";
 
 class Header extends React.Component {
-    render() {
-        return (
-            <Nav css={headerStyles} defaultActiveKey="/home" as="ul">
-            <Nav.Item as="li">
-              <Nav.Link href="/home">Active</Nav.Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-              <Nav.Link eventKey="link-1">Link</Nav.Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-              <Nav.Link eventKey="link-2">Link</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Nav style={headerStyles} defaultActiveKey="/home" as="ul">
+          <NavItem as="li">
+            <NavLink to="/">Home</NavLink>
+          </NavItem>
+          <NavItem as="li">
+            <NavLink to="/characters">Characters</NavLink>
+          </NavItem>
+          <NavItem as="li">
+            <NavLink to="/planets">Planets</NavLink>
+          </NavItem>
+        </Nav>
+
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Container />} />
+            <Route path="/planets" element={<Planets />} />
+            <Route path="/characters" element={<Characters />} />
+          </Routes>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Header;
